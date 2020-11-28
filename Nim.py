7dcf1569bbc8a288d8f06f4d7182facdd617dc18
tracybg <33481648+tracybg@users.noreply.github.com>
2020-11-28 13:07:32 +0800

    Create Nim.py

---
+++ Nim.py
@@ -0,0 +1,73 @@
+"""
+A simple Monte Carlo solver for Nim
+http://en.wikipedia.org/wiki/Nim#The_21_game
+"""
+
+import random
+import codeskulptor
+codeskulptor.set_timeout(20)
+
+MAX_REMOVE = 3
+TRIALS = 10000
+
+def evaluate_position(num_items):
+    """
+    Monte Carlo evalation method for Nim
+    """
+    
+    # Insert your code here
+    
+    initial_move = random.randrange(MAX_REMOVE)
+    items_left = num_items
+    
+    win_count = 0
+    
+    while True:
+        current_move = initial_move
+        items_left -= current_move
+        if items_left <= 0:
+            win_count += 1
+            
+            random.randrange(MAX_REMOVE)
+        
+        comp_move = evaluate_position(current_items)
+        current_items -= comp_move
+        print "Computer choose", comp_move, ", current value is", current_items
+        if current_items <= 0:
+            print "Computer wins"
+            break
+        player_move = int(input("Enter your current move"))
+        current_items -= player_move
+        print "Player choose", player_move, ", current value is", current_items
+        if current_items <= 0:
+            print "Player wins"
+            break        
+    num_items -= initial_move
+
+
+def play_game(start_items):
+    """
+    Play game of Nim against Monte Carlo bot
+    """
+    
+    current_items = start_items
+    print "Starting game with value", current_items
+    while True:
+        comp_move = evaluate_position(current_items)
+        current_items -= comp_move
+        print "Computer choose", comp_move, ", current value is", current_items
+        if current_items <= 0:
+            print "Computer wins"
+            break
+        player_move = int(input("Enter your current move"))
+        current_items -= player_move
+        print "Player choose", player_move, ", current value is", current_items
+        if current_items <= 0:
+            print "Player wins"
+            break
+
+play_game(21)
+        
+    
+                 
+    
